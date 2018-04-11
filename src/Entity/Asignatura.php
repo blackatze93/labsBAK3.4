a139e7f925b7d427693360b90468d77c0fde6154
Andres Sanchez <blackatze93@gmail.com>
2018-04-10 20:41:15 -0500

    Agregando asignatura

---
+++ src/Entity/Asignatura.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints as DoctrineAssert;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\AsignaturaRepository")
+ * @DoctrineAssert\UniqueEntity("codigo")
+ */
+class Asignatura
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(type="integer", unique=true)
+     * @Assert\NotBlank()
+     * @Assert\Range(min="0")
+     */
+    private $codigo;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", length=100)
+     * @Assert\NotBlank()
+     * @Assert\Length(max="100")
+     */
+    private $nombre;
+
+    /**
+     * @return string
+     */
+    public function __toString()
+    {
+        return $this->getNombre();
+    }
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return int
+     */
+    public function getCodigo()
+    {
+        return $this->codigo;
+    }
+
+    /**
+     * @param int $codigo
+     */
+    public function setCodigo($codigo)
+    {
+        $this->codigo = $codigo;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    /**
+     * @param string $nombre
+     */
+    public function setNombre($nombre)
+    {
+        $this->nombre = mb_convert_case($nombre, MB_CASE_TITLE, 'UTF-8');
+    }
+}
