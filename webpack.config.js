ae5b8a8d4267881fc1398a4925317221c9d9f329
Andres Sanchez <anfsanchezo@correo.udistrital.edu.co>
2021-05-16 22:59:04 +0000

    Actualizando composer y yarn

--- webpack.config.js
+++ webpack.config.js
@@ -32,6 +32,14 @@ Encore
 
     // enables hashed filenames (e.g. app.abc123.css)
     .enableVersioning(Encore.isProduction())
+
+    .copyFiles([
+        {from: './node_modules/ckeditor/', to: 'ckeditor/[path][name].[ext]', pattern: /\.(js|css)$/, includeSubdirectories: false},
+        {from: './node_modules/ckeditor/adapters', to: 'ckeditor/adapters/[path][name].[ext]'},
+        {from: './node_modules/ckeditor/lang', to: 'ckeditor/lang/[path][name].[ext]'},
+        {from: './node_modules/ckeditor/plugins', to: 'ckeditor/plugins/[path][name].[ext]'},
+        {from: './node_modules/ckeditor/skins', to: 'ckeditor/skins/[path][name].[ext]'}
+    ])
 ;
 
 module.exports = Encore.getWebpackConfig();
